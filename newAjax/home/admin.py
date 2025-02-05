940ab6cbb5f3a097d5385bdd9ca24f8e9ae1fd2a
prime-coder7 <shivshaktisweets123@gmail.com>
2025-02-05 21:59:04 +0530

    add

---
+++ newAjax/home/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from home.models import *
+
+# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "phone"]
+    
+admin.site.register(Student, StudentAdmin)
+    
