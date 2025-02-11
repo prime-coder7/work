3a193267e458c116332b20a9983566f3a71e8002
prime-coder7 <shivshaktisweets123@gmail.com>
2025-02-11 18:34:00 +0530

    add

--- Digital_Society/home/admin.py
+++ Digital_Society/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from home.models import *
 
 # Register your models here.
+admin.site.register(Chairman)
+admin.site.register(Notice)
+admin.site.register(Event)
+admin.site.register(Visitor)
+admin.site.register(Watchmen)
+
+
+
