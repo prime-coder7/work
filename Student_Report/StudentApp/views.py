d4f10e459db837de98404e806f9ed77247c42005
prime-coder7 <shivshaktisweets123@gmail.com>
2025-02-13 17:50:39 +0530

    add

--- Student_Report/StudentApp/views.py
+++ Student_Report/StudentApp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from StudentApp.models import *
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     students = Student.objects.all()
-    return render(request, "index.html", {"students" :students})
+    paginator = Paginator(students, 25)  # Show 25 contacts per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "index.html", {"students": page_obj})
 
 def marks(request, id):
     student = Student.objects.get(s_id__student_id=id)  # Fetch the student by student_id
